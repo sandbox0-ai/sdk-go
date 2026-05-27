e0fe29daca30325cd3320547475fa3c93f54f4e9
Zuch Huang <langyi.huang.1997@gmail.com>
2026-05-27 04:40:10 -0500

    Revert "Add sandbox function invocation (#45)"
    This reverts commit 301870d1f1f7088ecdebca9c38594eeaf9212a13.

--- pkg/apispec/oas_interfaces_gen.go
+++ pkg/apispec/oas_interfaces_gen.go
@@ -57,10 +57,6 @@ type APIV1SandboxesIDFilesPostRes interface {
 	aPIV1SandboxesIDFilesPostRes()
 }
 
-type APIV1SandboxesIDFunctionsNameInvokePostRes interface {
-	aPIV1SandboxesIDFunctionsNameInvokePostRes()
-}
-
 type APIV1SandboxesIDGetRes interface {
 	aPIV1SandboxesIDGetRes()
 }
