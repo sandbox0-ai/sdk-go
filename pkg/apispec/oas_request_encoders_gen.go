301870d1f1f7088ecdebca9c38594eeaf9212a13
Zuch Huang <zhihao.huang.1997@gmail.com>
2026-05-27 16:11:27 +0800

    Add sandbox function invocation (#45)
    Co-authored-by: Zuch Huang <langyi.huang.1997@gmail.com>

--- pkg/apispec/oas_request_encoders_gen.go
+++ pkg/apispec/oas_request_encoders_gen.go
@@ -180,6 +180,20 @@ func encodeAPIV1SandboxesIDFilesPostRequest(
 	return nil
 }
 
+func encodeAPIV1SandboxesIDFunctionsNameInvokePostRequest(
+	req *FunctionInvokeRequest,
+	r *http.Request,
+) error {
+	const contentType = "application/json"
+	e := new(jx.Encoder)
+	{
+		req.Encode(e)
+	}
+	encoded := e.Bytes()
+	ht.SetBody(r, bytes.NewReader(encoded), contentType)
+	return nil
+}
+
 func encodeAPIV1SandboxesIDNetworkPutRequest(
 	req *SandboxNetworkPolicy,
 	r *http.Request,
